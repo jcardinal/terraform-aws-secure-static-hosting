a9df064f1a5de993683cff89813d9aef2cba3194
Justin Cardinal <justin@somuchuptime.com>
2020-05-09 13:32:05 -0500

    Ensures ACM cert is validated before creating CloudFront distribution

--- main.tf
+++ main.tf
@@ -35,6 +35,11 @@ resource "aws_route53_record" "domain_validation" {
   ttl     = "600"
 }
 
+resource "aws_acm_certificate_validation" "validation" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [aws_route53_record.domain_validation.fqdn]
+}
+
 resource "aws_cloudfront_distribution" "distribution" {
   aliases         = [var.domain_name]
   price_class     = "PriceClass_100"
@@ -80,7 +85,7 @@ resource "aws_cloudfront_distribution" "distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.certificate.arn
+    acm_certificate_arn      = aws_acm_certificate_validation.validation.certificate_arn
     minimum_protocol_version = "TLSv1.2_2018"
     ssl_support_method       = "sni-only"
   }
