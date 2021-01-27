a88f0f63911a37add9a1dfc232a0a8e6f388039b
Justin Cardinal <justin@somuchuptime.com>
2021-01-27 16:31:43 -0600

    Upgrade to AWS provider version 3

--- main.tf
+++ main.tf
@@ -29,16 +29,24 @@ resource "aws_acm_certificate" "certificate" {
 }
 
 resource "aws_route53_record" "domain_validation" {
-  zone_id = data.aws_route53_zone.zone.zone_id
-  type    = aws_acm_certificate.certificate.domain_validation_options[0].resource_record_type
-  name    = aws_acm_certificate.certificate.domain_validation_options[0].resource_record_name
-  records = [aws_acm_certificate.certificate.domain_validation_options[0].resource_record_value]
-  ttl     = "600"
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.zone.zone_id
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
+  ttl             = "600"
 }
 
 resource "aws_acm_certificate_validation" "validation" {
   certificate_arn         = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [aws_route53_record.domain_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.domain_validation : record.fqdn]
 }
 
 resource "aws_cloudfront_distribution" "distribution" {
